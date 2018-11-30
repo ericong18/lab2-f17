78f4586d9516992967f6df8c4dd6f5a0a62750e6
Eric Ong <eong001@sledge.cs.ucr.edu>
2018-11-30 09:53:08 -0800

    Almost done with case 2

--- shm.c
+++ shm.c
@@ -35,26 +35,45 @@ int shm_open(int id, char **pointer) {
   acquire(&(shm_table.lock));
 
   for (i = 0; i < 64; i++) { 
-   if (shm_table.shm_pages[i].id == id) {
-     uint pa = &shm_table.shm_pages[i].frame;
-     uint va = PGROUNDUP(myproc()->sz);
+    if (shm_table.shm_pages[i].id == id) {
+      uint pa = (uint)&shm_table.shm_pages[i].frame;
+      uint va = PGROUNDUP(myproc()->sz);
 
-     if (mappages(myproc()->pgdir, va, PGSIZE, pa, PTE_W|PTE_U) == 0) {
-       goto bad;
-     }
-     shm_table.shm_pages[i].refcnt += 1;
-     *pointer=(char *)va;
-     myproc()->sz += PGSIZE;
-   }
-   else { // shared memory segment does not exist
-     if (!shm_table.shm_pages[i].frame) { // if we find an empty entry
-     
-     }
-   }
+      if (mappages(myproc()->pgdir, (void *)va, PGSIZE, pa, PTE_W|PTE_U) == 0) {
+        goto bad;
+      }
+
+      shm_table.shm_pages[i].refcnt += 1;
+      *pointer = (char *)va;
+      myproc()->sz += PGSIZE;
+    }
+    else { // shared memory segment does not exist
+      if (!shm_table.shm_pages[i].frame) { // if we find an empty entry
+        shm_table.shm_pages[i].id = id;
+        char * ka = kmalloc(PGSIZE, GFP_KERNEL);
+
+        if (!ka) {
+          goto bad;
+        }
+        shm_table.shm_pages[i].frame = ka;
+        shm_table.shm_pages[i].refcnt = 1;
+
+        uint pa = (uint)&shm_table.shm_pages[i].frame;
+        uint va = PGROUNDUP(myproc()->sz);
+
+        if (mappages(myproc()->pgdir, (void *)va, PGSIZE, pa, PTE_W|PTE_U) == 0) {
+          goto bad;
+        }
+
+      }
+    }
   }
 
   release(&(shm_table.lock));
-
+  
+  bad:
+    exit();
+  
   return 0; //added to remove compiler warning -- you should decide what to return
 }
 
